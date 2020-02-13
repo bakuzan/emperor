968de185c3c47ad0aeeebf683e30332ae477db20
bakuzan <imannoyed4days@hotmail.co.uk>
2020-02-13 11:27:43 +0000

    browser code.

---
+++ gatsby-brower.js
@@ -0,0 +1,39 @@
+function getTargetOffset(hash) {
+  const id = window.decodeURI(hash.replace(`#`, ``));
+
+  if (id !== ``) {
+    const element = document.getElementById(id);
+
+    if (element) {
+      return element.offsetTop;
+    }
+  }
+
+  return null;
+}
+
+export function onInitialClientRender() {
+  requestAnimationFrame(() => {
+    const offset = getTargetOffset(window.location.hash);
+
+    if (offset !== null) {
+      window.scrollTo(0, offset);
+    }
+  });
+}
+
+export function shouldUpdateScroll({ routerProps: { location } }) {
+  const offset = getTargetOffset(location.hash);
+  return offset !== null ? [0, offset] : true;
+}
+
+export function onServiceWorkerUpdateReady() {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  );
+
+  if (answer === true) {
+    window.location.reload();
+  }
+}
